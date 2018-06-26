b710a29192ee2e1267f130494d674f7bc7a6c7b7
aepayne3 <aepayne3@crimson.ua.edu>
2018-06-26 16:39:13 -0500

    Add files via upload

---
+++ BinarySearchDeluxe.java
@@ -0,0 +1,11 @@
+public class BinarySearchDeluxe {
+
+    // Returns the index of the first key in a[] that equals the search key, or -1 if no such key.
+    public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator)
+
+    // Returns the index of the last key in a[] that equals the search key, or -1 if no such key.
+    public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator)
+
+    // unit testing (required)
+    public static void main(String[] args)   
+}
