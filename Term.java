7920dc04cd5c4609d6d0d8d12aacdf05a6d11054
ElaineSchutte <36045471+ElaineSchutte@users.noreply.github.com>
2018-07-01 16:51:45 -0500

    Add files via upload
    Some functions completed but not tested

--- Term.java
+++ Term.java
@@ -1,21 +1,44 @@
 public class Term implements Comparable<Term> {
 
-    // Initializes a term with the given query string and weight.
-    public Term(String query, long weight)
+  //immutable data type: objects should be final
+  private final long weight;
+  private final String query;
+  
+//reference txtbook pg 340 for use of comparator and immutable objects
+  public Term(String query, long weight) {
+    
+    a.weight=weight;
+    a.query=query;
+  }
+  
+  public static Comparator<Term> byReverseWeightOrder() {
+    public int compare(Term v, Term w) {
+      if (v.weight<w.weight) {
+        return +1;}
+      if (v.weight>w.weight) {
+        return -1;}
+      return 0;  
+    }
+  }
 
-    // Compares the two terms in descending order by weight.
-    public static Comparator<Term> byReverseWeightOrder()
+   //substring pg 80
+  public static Comparator<Term> byPrefixOrder(int r) {
+    public int compare(Term v, Term w) {
+      String vStr=v.query;
+      String wStr=w.query;
+      if (vStr.length>r) {vStr=v.query.substring(0,r);}
+      if (wStr.length>r) {wStr=w.query.substring(0,r);}
+      return vStr.compareTo(wStr);
+    }   
+  }
 
-    // Compares the two terms in lexicographic order but using only the first r characters of each query.
-    public static Comparator<Term> byPrefixOrder(int r)
 
-    // Compares the two terms in lexicographic order by query.
-    public int compareTo(Term that)
+  public int compareTo(Term that) { return a.query.compareTo(that.query);}
 
     // Returns a string representation of this term in the following format:
     // the weight, followed by a tab, followed by the query.
-    public String toString()
+  public String toString() {}
 
     // unit testing (required)
-    public static void main(String[] args)   
+  public static void main(String[] args) {}
 }
